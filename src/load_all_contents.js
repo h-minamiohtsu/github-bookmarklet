71368940003856d5374c564d31b2d1063e3a6fd0
h-minamiohtsu <h.minamiohtsu@cartahd.com>
2024-11-18 19:47:13 +0900

    update

--- src/load_all_contents.js
+++ src/load_all_contents.js
@@ -1,9 +1,11 @@
-function clickLoadButton() {
-    const button = document.querySelector('button[data-testid="issue-timeline-load-more-front"]');
-    if (button) {
-        button.click();
-        setTimeout(clickLoadButton, 1500);
+(function(){
+    function clickLoadButton() {
+        const button = document.querySelector('button[data-testid="issue-timeline-load-more-front"]');
+        if (button) {
+            button.click();
+            setTimeout(clickLoadButton, 1500);
+        }
     }
-}
 
-clickLoadButton();
+    clickLoadButton();
+})();
--- src/resize_image.js
+++ src/resize_image.js
@@ -1,13 +1,9 @@
 function replaceImgTag(targetElement) {
-    const githubImgRegexp = /!\[image\]\(([^)]+)\)/g;
+    const githubImgRegexp = /!\[Image\]\(([^)]+)\)/g;
     const replacedComment = targetElement.value.replace(githubImgRegexp, '<img src="$1" width="300px">');
     targetElement.value = replacedComment;
 }
 
-document.getElementsByName("comment[body]").forEach((element) => {
-    replaceImgTag(element)
-});
-
-document.getElementsByName("issue_comment[body]").forEach((element) => {
+document.querySelectorAll("textarea").forEach((element) => {
     replaceImgTag(element)
 });
